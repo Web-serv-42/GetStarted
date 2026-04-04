acf32e033545e3b9938f1b22779ff4b5d753b87c
Abdellah-Nsila <dawndawn074@gmail.com>
2026-04-04 18:10:03 +0100

    socket  + bind

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <cstdlib>
 
 #include <sys/types.h>
 #include <sys/socket.h>
+
 #include <netdb.h>
 
 int main(int argc, char const *argv[])
@@ -14,16 +16,29 @@ int main(int argc, char const *argv[])
 	struct addrinfo		hints;
 	struct addrinfo*	servinfo;  // will point to the results
 	int					status;
+	int					sockfd;
 
-	memset(&hints, 0, sizeof hints); // make sure the struct is empty
-	hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
-	hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+	memset(&hints, 0, sizeof(hints)); // make sure the struct is empty
+	hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6 => AF_INET AF_INET6
+	hints.ai_socktype = SOCK_STREAM; // TCP stream sockets. Or SOCK_DGRAM for UDP
 	hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 	
 	if  ((status = getaddrinfo(NULL, "1337", &hints, &servinfo)) != 0)
 	{
 		std::cerr << gai_strerror(status) << std::endl;
+		exit(1);
 	}
+
+	sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+
+	status = bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
+	if (status != 0)
+	{
+		std::cerr << gai_strerror(status) << std::endl;
+		exit(1);
+	}
+
 	freeaddrinfo(servinfo);
 	return 0;
 }
