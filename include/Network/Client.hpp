de85a03856930542cf4ae49c6596ef89555e9cf5
Abdellah-Nsila <dawndawn074@gmail.com>
2026-04-27 18:56:40 +0100

    Prepare client to manage request/response

--- include/Network/Client.hpp
+++ include/Network/Client.hpp
@@ -6,21 +6,40 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 16:57:35 by abnsila           #+#    #+#             */
-/*   Updated: 2026/04/25 18:29:22 by abnsila          ###   ########.fr       */
+/*   Updated: 2026/04/25 19:23:16 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include "Core/Log.hpp"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 class Client
 {
 	private:
-		int	m_SocketFd;
+		int						m_SocketFd;
+		struct sockaddr_storage	m_ClientAddr;
+
+		// Request  m_Request;   <-- Later: HTTP Request Parser
+    	// Response m_Response;  <-- Later: HTTP Response Builder
+
+		// Buffers to hold data if recv/send are interrupted (Non-blocking)
+		std::string				m_ReadBuffer;
+		std::string				m_WriteBuffer;
 	public:
 		Client();
-		Client(int clientFd);
+		Client(int clientFd, struct sockaddr_storage m_ClientAddr);
 		~Client();
 
-		int	GetClientFd() const;
-};
+		bool	ReadData();
+		bool	SendData();
+		// Later I will add methods like:
+   		// void BuildResponse();
 
+		void	DisplayClientInfo() const;
+		int		GetClientFd() const;
+};
--- src/Network/Client.cpp
+++ src/Network/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: abnsila <abnsila@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 16:57:53 by abnsila           #+#    #+#             */
-/*   Updated: 2026/04/25 18:29:36 by abnsila          ###   ########.fr       */
+/*   Updated: 2026/04/25 19:23:52 by abnsila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,24 @@ Client::Client()
 {
 }
 
-Client::Client(int clientFd) : m_SocketFd(clientFd)
+Client::Client(int clientFd, struct sockaddr_storage clientAddr)
+	: m_SocketFd(clientFd), m_ClientAddr(clientAddr)
 {
+	this->DisplayClientInfo();
 }
 
 Client::~Client()
 {
+}
+
+bool	Client::ReadData()
+{
+	return (true);
+}
+
+bool	Client::SendData()
+{
+	return (true);
 }
 
 int	Client::GetClientFd() const
@@ -29,3 +41,28 @@ int	Client::GetClientFd() const
 	return (this->m_SocketFd);
 }
 
+void	Client::DisplayClientInfo() const
+{
+	char 					str[INET6_ADDRSTRLEN];
+	struct sockaddr_in*		ptr = NULL;
+	struct sockaddr_in6*	ptr1 = NULL;
+
+	// Print IP address of the new client
+	if (this->m_ClientAddr.ss_family == AF_INET)
+	{
+		ptr = (struct sockaddr_in *) &this->m_ClientAddr;
+		inet_ntop(AF_INET, &(ptr->sin_addr), str, sizeof(str));
+	}
+	else if (this->m_ClientAddr.ss_family == AF_INET6)
+	{
+		ptr1 = (struct sockaddr_in6 *) &this->m_ClientAddr;
+		inet_ntop(AF_INET6, &(ptr1->sin6_addr), str, sizeof(str));
+	}
+	else
+	{
+		ptr = NULL;
+		TRACE_LOG("Address family is neither AF_INET nor AF_INET6");
+	}
+	if (ptr) 
+		INFO_LOG("Connection from client: " + std::string(str));
+}
